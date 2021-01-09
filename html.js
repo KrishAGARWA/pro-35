e787a3a451c9149884de1ece48c61fad08e77933
KrishAGARWA <73591403+KrishAGARWA@users.noreply.github.com>
2021-01-09 19:42:47 +0530

    Add files via upload

---
+++ html.js
@@ -0,0 +1,29 @@
+class Form{
+
+constructor(){
+//this.input=createInput()
+
+
+
+}
+
+
+display(){
+    this.button=createButton("Add Food")
+this.button.position(250,200)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
